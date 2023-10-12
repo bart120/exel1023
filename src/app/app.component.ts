435f76bd6f44298f35922c75cf2fe33122528e19
Vincent Leclerc <vleclerc@inow.fr>
2023-10-12 09:22:31 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'exel1023';
+  title = 'exel1024';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-file',
+
+      },
+      {
+        label: 'Voitures',
+        icon: 'pi pi-fw pi-pencil',
+        items: [
+          {
+            label: 'Ajouter',
+            icon: 'pi pi-fw pi-align-left'
+          },
+          {
+            label: 'Lister',
+            icon: 'pi pi-fw pi-align-right'
+          }
+        ]
+      },
+      {
+        label: 'Connexion',
+        icon: 'pi pi-fw pi-file',
+
+      },
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MenubarModule } from 'primeng/menubar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
